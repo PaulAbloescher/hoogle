de121f85203f2e3ef4bd46d0abd017aacb93585a
PaulAbloescher <abloescherpaul@gmail.com>
2022-04-15 19:39:44 +0200

    silence plugin demo

--- src/Plugin/PluginDemo.hs
+++ src/Plugin/PluginDemo.hs
@@ -1,5 +1,5 @@
-{-# OPTIONS -fplugin=Plugin.HolePlugin -fplugin-opt=Plugin.HolePlugin:http://localhost:8000 -funclutter-valid-hole-fits #-}
-{-# OPTIONS -fmax-valid-hole-fits=10 #-}
+-- {-# OPTIONS -fplugin=Plugin.HolePlugin -fplugin-opt=Plugin.HolePlugin:http://localhost:8000 -funclutter-valid-hole-fits #-}
+-- {-# OPTIONS -fmax-valid-hole-fits=10 #-}
 {-# LANGUAGE OverloadedStrings #-}
 module Plugin.PluginDemo where
 
@@ -7,8 +7,11 @@ import Plugin.HolePlugin ()
 import Data.Char (toUpper, isUpper)
 import Data.Text
 
-test_one :: Text -> Text 
-test_one x = _remove_leading_whitespace x
+-- test_one :: Text -> Text 
+-- test_one x = _remove_leading_whitespace x
 
-test_two :: Text -> Text 
-test_two x = _turn_around x
+-- test_two :: [a] -> [a] 
+-- test_two x = _turn_around x
+
+-- autoTyInference :: [a] -> a
+-- autoTyInference xs = _get_first_element xs 1
