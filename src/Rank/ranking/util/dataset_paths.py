611cdc7196ddf56dfe8c8212bc6822d7d513032c
PaulAbloescher <abloescherpaul@gmail.com>
2022-04-17 09:43:50 +0200

    update statistics and data paths.

--- src/Rank/ranking/util/dataset_paths.py
+++ src/Rank/ranking/util/dataset_paths.py
@@ -2,8 +2,8 @@ import os
 from pathlib import Path
 
 base_dir =  Path(__file__).parents[2]
-corpora_dir = os.path.join(base_dir, 'datasets\\corpora\\without-code-examples')
-evaluation_dir = os.path.join(base_dir, 'datasets\\evaluation')
+corpora_dir = os.path.join(base_dir, 'datasets\\corpora\\complete-without-code-examples')
+evaluation_dir = os.path.join(base_dir, 'datasets\\evaluation\\complete')
 
 # Corpora
 lemmatized_unique_functions_corpus = os.path.join(corpora_dir, 'lem-all-unique-functions.jsonl')
@@ -14,7 +14,7 @@ lemmatized_unique_sentences_corpus = os.path.join(corpora_dir, 'lemmatized-corpu
 tokenized_unique_sentences_corpus = os.path.join(corpora_dir, 'tokenized-corpus.txt')
 
 unique_functions_corpus = os.path.join(corpora_dir, 'all-unique-functions.jsonl')
-raw_corpus = os.path.join(corpora_dir, 'no-code-ex-raw.dump.jsonl')
+# raw_corpus = os.path.join(corpora_dir, 'no-code-ex-raw.dump.jsonl')
 
 # Evaluation
 lemmatized_tfidf_evaluation_set = os.path.join(evaluation_dir, 'lemmatized-eval-plain-hoogle-results.jsonl')
