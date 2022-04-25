18995b2f6515ff3f9df096b714ceb311978dc3c2
PaulAbloescher <abloescherpaul@gmail.com>
2022-04-25 20:44:57 +0200

    parse signatures during extraction

--- src/Rank/ranking/util/dataset_paths.py
+++ src/Rank/ranking/util/dataset_paths.py
@@ -2,8 +2,8 @@ import os
 from pathlib import Path
 
 base_dir =  Path(__file__).parents[2]
-corpora_dir = os.path.join(base_dir, 'datasets\\corpora\\complete-without-code-examples')
-evaluation_dir = os.path.join(base_dir, 'datasets\\evaluation\\complete')
+corpora_dir = os.path.join(base_dir, 'datasets\\corpora\\parsed-without-code')
+# evaluation_dir = os.path.join(base_dir, 'datasets\\evaluation\\complete')
 
 # Corpora
 lemmatized_unique_functions_corpus = os.path.join(corpora_dir, 'lem-all-unique-functions.jsonl')
@@ -19,6 +19,6 @@ unique_functions_corpus = os.path.join(corpora_dir, 'all-unique-functions.jsonl'
 raw_corpus = os.path.join(corpora_dir, 'no-code-ex-raw.dump.jsonl')
 
 # Evaluation
-lemmatized_tfidf_evaluation_set = os.path.join(evaluation_dir, 'lemmatized-eval-plain-hoogle-results.jsonl')
-tokenized_tfidf_evaluation_set = os.path.join(evaluation_dir, 'tokenized-eval-plain-hoogle-results.jsonl')
-manual_evaluation_set = os.path.join(evaluation_dir, 'ManualEvalSet.txt')
+# lemmatized_tfidf_evaluation_set = os.path.join(evaluation_dir, 'lemmatized-eval-plain-hoogle-results.jsonl')
+# tokenized_tfidf_evaluation_set = os.path.join(evaluation_dir, 'tokenized-eval-plain-hoogle-results.jsonl')
+# manual_evaluation_set = os.path.join(evaluation_dir, 'ManualEvalSet.txt')
